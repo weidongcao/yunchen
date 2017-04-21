ecc5ebcb0c40c02e39e30c43a926afbc01fd052b
yunchen <sh_caoweidong@nebusoft.com.cn>
2017-04-21 11:37:56 +0800

    项目创建及小区人群分析功能的开发

--- src/main/com/rainsoft/j2se/IntTest.java
+++ src/main/com/rainsoft/j2se/IntTest.java
@@ -1,5 +1,13 @@
 package com.rainsoft.j2se;
 
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017-04-14.
  */
@@ -15,5 +23,52 @@ public class IntTest {
      */
     public static Integer getMaxInt() {
         return Integer.MAX_VALUE;
+    }
+
+    /**
+     * Created by Administrator on 2017-04-20.
+     */
+    public static class JSONTest {
+        public static void main(String[] args) {
+            Map<String, Integer> map = new HashMap<>();
+
+            map.put("aaa", 4);
+            map.put("bbb", 40);
+            map.put("ccc", 400);
+
+            JSONObject jsonObject = new JSONObject(map);
+            System.out.println(jsonObject.toString());
+        }
+    }
+
+    /**
+     * Created by Administrator on 2017-04-17.
+     */
+    public static class NumTest {
+        public static void main(String[] args) {
+            Float f1 = 0.34f;
+            Float f2 = 0.44f;
+            Float f3 = 0.55f;
+            System.out.println("Math.floor(f1) = " + Math.floor(f1));
+            System.out.println("Math.rint(f3) = " + Math.rint(f3));
+            System.out.println(f2 + "");
+        }
+    }
+
+    /**
+     * Created by Administrator on 2017-04-11.
+     */
+    public static class TimeTest {
+        public static void main(String[] args) {
+            SimpleDateFormat dataFormat = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss");
+            Calendar cala = Calendar.getInstance();
+            Date cur = cala.getTime();
+            System.out.println("cala = " + cala);
+            System.out.println(dataFormat.format(cur) + " --> " + cur.getTime());
+            cala.add(Calendar.DATE, -1);
+            Date yes = cala.getTime();
+            System.out.println(dataFormat.format(yes) + " --> " + yes.getTime());
+
+        }
     }
 }
